a2740d6efae32bd3cd65a8f5445ea636fd1e4ed5
Leon_Martinez <70551293+LeonMartinez23@users.noreply.github.com>
2022-03-23 22:45:01 -0600

    Add files via upload
    First commit

---
+++ Nahual/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+        <head>
+            <title>Nahual Live Rock</title>
+            <link rel="icon" type="image/x-icon" href="img/icono.png">
+            <meta charset="UTF-8"/>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <link rel="stylesheet" href="css/estilos.css">
+        </head>
+    <body>
+        
+        <div style="background-color: rgba(0, 0, 0, 0.2);"><img id="logo" img src="img/logoNahualBlanco - Letras.png" width="26%">
+        </div>
+
+        <div class="container text-center">
+            <h2>Trayectoria</h2>
+
+            <p>
+                Además de la entrañable amistad y hermandad, Nahual tiene trayectoria desde bares como lo son:</p>
+                <ul>
+                    <li>Central Wings </li>
+                    <li>Bar Bartola</li>
+                    <li>Pisto's Burguers (Cadena del Rock)</li>
+                </ul>
+                
+                <p>hasta eventos privados, como los hemos tenido en cumpleaños, fiestas empresariales, reuniones sociales, acoplandonos a todo tipo de público.
+            </p>
+            
+            
+        
+        </div>
+        
+        <img src="img/all.jpg" width="100%">
+        
+        <div class="hero-image">
+
+                <div class="container py-3">
+
+                    <div class="row py-2">
+                        <div class="col-12">
+                            <h1 class="text-center">Nuestra integracion cuenta con 4 musicos principales</h1></div>
+                        </div>
+
+                    <div class="row py-2">
+
+                        <div class="col-sm-3">
+                            <img src="img/11.jpg" width="100%">
+                        </div>
+                        <div class="col-sm-3">
+                            <p>MOISES VERA Voz principal, coros, y guitarra ritmica.</p>
+                        </div>
+
+                        <div class="col-sm-3">
+                            <img src="img/12.jpg" width="100%">
+                        </div>
+                        <div class="col-sm-3">
+                            <p>LEON LOPEZ Guitarra Lider.</p>
+                        </div>
+                    </div>
+
+                    <div class="row py-2">
+                    <div class="col-sm-3">
+                            <img src="img/13.jpg" width="100%">
+                        </div>
+                        <div class="col-sm-3">
+                            <p>FERNADO CISNEROS Bajo y coros.</p>
+                        </div>
+
+                        <div class="col-sm-3">
+                            <img src="img/15.jpg" width="100%">
+                        </div>
+                        <div class="col-sm-3">
+                            <p>GABO Bateria y coros.</p>
+                        </div>
+                    </div>
+
+                    
+                </div>
+
+        </div>
+
+
+        <div class="container py-2">
+            <div><h2 class="text-center">Repertorio</h2></div>
+            <div class="row">
+                <div class="col-6 col-sm-8">
+
+                    <p>
+                        Nuestro repertorio abarca desde rock en inglés, español, heav y metal, hardrock, pop en español e ingles, grunge, dance, funk, cumbias, ska. Salsa,  Siendo así, completando un tiempo de 4 horas de repertorio para cualquier tipo de evento y presupuesto.
+                    </p>
+                </div>
+
+                <div class="col-6 col-sm-4">
+                        <img src="img/24.png" width="100%">
+                </div>
+
+            </div>
+
+        </div>
+        
+        <footer class="text-center text-white" style="background-color: rgba(0, 0, 0, 0.2);">
+            <div class="footer-image">
+                <div class="container py-5">
+                    <div class="row">
+                
+                        <div class="col-4 p-2">
+                            <!-- Facebook -->
+                            <a href="https://www.facebook.com/NahualRockBanda/" role="button"><img src="img/25.png" width="30%"></a> 
+                        </div>
+
+                        <div class="col-4 p-2">
+
+                            <!-- Instagram -->
+                            <a href="https://www.instagram.com/nahual_live_rock/?hl=es" role="button"><img src="img/26.png" width="30%"></a> 
+                    
+                        </div>
+
+                        <div class="col-4 p-2">
+
+                        <!-- Instagram -->
+                        <a href="https://wa.me/524776644814"><img src="img/30.png" width="30%"></a> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+                
+                <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+                © <?php echo date("Y");?> Nahual Live Rock
+                </div>
+            
+        </footer>
+        
+    </body>
+</html>
